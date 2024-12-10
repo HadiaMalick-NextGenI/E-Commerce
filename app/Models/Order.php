388befe4722f9e222cdf49e034450fa6977025d5
Hadia Malick <hadia.malick@nextgeni.com>
2024-12-10 11:43:59 +0500

    Added unique reference id for orders

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,10 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::creating(function ($order) {
+            do {
+                $referenceId = strtoupper(Str::random(10));
+            } while (self::where('reference_id', $referenceId)->exists());
+    
+            $order->reference_id = $referenceId;
+        });
+    }
 
     protected $fillable = [
         'user_id',
