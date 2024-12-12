34e07c54c5418e22aa655ca610ee08f4d1ca0560
Hadia Malick <hadia.malick@nextgeni.com>
2024-12-12 09:39:24 +0500

    Helper function to get price based on on_sale attribute

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Collection;
 
+if (!function_exists('getProductPrice')) {
+    function getProductPrice($product): float
+    {
+        return $product->on_sale ? $product->discounted_price : $product->price;
+    }
+}
+
 if (!function_exists('calculateTotalAmount')) {
     function calculateTotalAmount(Collection $cartItems): float
     {
         return $cartItems->sum(function ($item) {
-            return $item->quantity * $item->product->price;
+            return $item->quantity * getProductPrice($item->product);
         });
     }
 }
